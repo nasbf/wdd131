90c2282bc8e0647305d4bdbb65df3f3dffc9a028
nasbf <mo0ma@hotmail.com>
2024-11-09 22:40:32 -0500

    javascript

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,3 +1,12 @@
 
 
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+const lastModifiedElement = document.getElementById("lastModified");
+const currentYearElement = document.getElementById("year");
+
+
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = `© ${currentYear} Alexandra Suárez, Bogotá - Colombia`;
+
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Última modificación: ${lastModifiedDate}`;
+
