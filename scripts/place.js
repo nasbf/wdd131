cbf00969a14976aa0f822c1ec5406fda6d2e7951
nasbf <mo0ma@hotmail.com>
2024-11-21 13:45:48 -0500

    mobile view place

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+const lastModifiedElement = document.getElementById("lastModified");
+const currentYearElement = document.getElementById("year");
+
+
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = `© ${currentYear} Alexandra Suárez, Bogotá - Colombia`;
+
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
