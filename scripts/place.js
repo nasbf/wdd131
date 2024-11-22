c512fb1ea0c2c88aa1be8bd6211a4e1bf9098831
nasbf <mo0ma@hotmail.com>
2024-11-22 08:22:01 -0500

    winchill

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,17 @@ currentYearElement.textContent = `© ${currentYear} Alexandra Suárez, Bogotá -
 
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+
+
+
+let result = document.getElementById("windchill");
+
+
+function calculateWindchill(temp, wind) {
+    let windchill = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(wind, 0.16)) + (0.3965 * temp * Math.pow(wind, 0.16));
+    return windchill.toFixed(2);
+}
+
+let temp = 13;
+let wind = 3;
+result.textContent = `Windchill: ${calculateWindchill(temp, wind)}℃`;
